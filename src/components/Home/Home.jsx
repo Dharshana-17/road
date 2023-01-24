3b9da29b7f70df1477ac7955ed66ba5f0661df0b
JasonKiptoo <kiptoojason@gmail.com>
2023-01-24 15:38:48 +0300

    first init worked on the navbar

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.css"
+
+function Home() {
+    return ( <div className="bg"></div> );
+}
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.css";
+import {Link} from "react-router-dom";
+
+function Navbar() {
+	const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav");
+	};
+
+	return (
+		<div className="navbar  mt-4">
+		<header >
+			{/* <h3>LOGO</h3> */}
+			<img style={{height: "50px"}} src="https://pps.whatsapp.net/v/t61.24694-24/322460561_140629475501171_8142685504268003046_n.jpg?ccb=11-4&oh=01_AdRc-8TUVq9q9fbd7Un54Y3i-RhEI_14bHK4n1Gn7uS0yA&oe=63DBA97B"/>
+			<nav ref={navRef}>
+				<Link ><a href="/#">Home</a></Link>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className="nav-btn" onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
+		</div>
+	);
+}
+
+export default Navbar;
