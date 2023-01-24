edf2d30c23700ec0f45360a61671177f6d1d7ea3
JasonKiptoo <kiptoojason@gmail.com>
2023-01-24 16:30:59 +0300

    added the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,18 +11,18 @@ function Navbar() {
 	};
 
 	return (
-		<div className="navbar  mt-4">
+		// <div className="navbar mt-4">
 		<header >
 			{/* <h3>LOGO</h3> */}
 			<img style={{height: "50px"}} src="https://pps.whatsapp.net/v/t61.24694-24/322460561_140629475501171_8142685504268003046_n.jpg?ccb=11-4&oh=01_AdRc-8TUVq9q9fbd7Un54Y3i-RhEI_14bHK4n1Gn7uS0yA&oe=63DBA97B"/>
 			<nav ref={navRef}>
-				<Link ><a href="/#">Home</a></Link>
+				<a href="/#">Home</a>
 				<a href="/#">My work</a>
 				<a href="/#">Blog</a>
 				<a href="/#">About me</a>
 				<button
 					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
+					onClick={showNavbar} >
 					<FaTimes />
 				</button>
 			</nav>
@@ -30,7 +30,7 @@ function Navbar() {
 				<FaBars />
 			</button>
 		</header>
-		</div>
+		// </div>
 	);
 }
 
