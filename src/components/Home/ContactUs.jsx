ebf2d784d3254fd9b96eac6c20b9f897ebe3f3c6
JasonKiptoo <kiptoojason@gmail.com>
2023-01-25 12:47:45 +0300

    changed the contact us icons

--- src/components/Home/ContactUs.jsx
+++ src/components/Home/ContactUs.jsx
@@ -8,7 +8,7 @@ function ContactUs() {
           <p class="fw-bold text-primary text-uppercase letter-spacing-3">
             Call me, maybe.
           </p>
-          <h2 class="text-uppercase lined">Contact</h2>
+          <h2 class="text lined pt-5">Get in touch with us today</h2>
         </header>
         <div class="row gy-4">
           <div class="col-lg-3 col-md-6">
@@ -18,7 +18,7 @@ function ContactUs() {
             >
               <i class="fas fa-map-marker-alt fa-2x mb-4"></i>
               <h4 class="contact-item-title h5 text-uppercase">Location</h4>
-              <p class="text-sm mb-0">Portland, Oregon</p>
+              <p class="text-sm mb-0">Nairobi, Kenya</p>
             </a>
           </div>
           <div class="col-lg-3 col-md-6">
@@ -26,9 +26,9 @@ function ContactUs() {
               class="px-4 py-5 text-center contact-item shadow-sm reset-anchor d-block"
               href="tel:534456886"
             >
-              <i class="fas fa-map-marker-alt fa-2x mb-4"></i>
+             <i class="fa fa-phone fa-2x mb-4" aria-hidden="true"></i>
               <h4 class="contact-item-title h5 text-uppercase">Phone</h4>
-              <p class="text-sm mb-0">534 456 886</p>
+              <p class="text-sm mb-0">0712345678</p>
             </a>
           </div>
           <div class="col-lg-3 col-md-6">
@@ -36,9 +36,9 @@ function ContactUs() {
               class="px-4 py-5 text-center contact-item shadow-sm reset-anchor d-block"
               href="www.example.com"
             >
-              <i class="fas fa-map-marker-alt fa-2x mb-4"></i>
+              <i class="fa fa-globe fa-2x mb-4"></i>
               <h4 class="contact-item-title h5 text-uppercase">Website</h4>
-              <p class="text-sm mb-0">www.example.com</p>
+              <p class="text-sm mb-0">www.siran.co.ke</p>
             </a>
           </div>
           <div class="col-lg-3 col-md-6">
@@ -46,9 +46,9 @@ function ContactUs() {
               class="px-4 py-5 text-center contact-item shadow-sm reset-anchor d-block"
               href="mailto:info@example.com"
             >
-              <i class="fas fa-map-marker-alt fa-2x mb-4"></i>
+             <i class="fa fa-envelope fa-2x mb-4" aria-hidden="true"></i>
               <h4 class="contact-item-title h5 text-uppercase">Email</h4>
-              <p class="text-sm mb-0">info@example.com</p>
+              <p class="text-sm mb-0">info@siran.com</p>
             </a>
           </div>
         </div>
